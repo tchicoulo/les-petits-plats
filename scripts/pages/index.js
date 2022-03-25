c45f9bfe28bff2b1bb7d086a9d6ed80666739d82
Calixte <tchicoulo@hotmail.fr>
2022-03-25 17:35:00 +0100

    OpenFilter function in progress

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,1 +1,15 @@
 console.log("ça fonctionne");
+
+const filterContainers = document.querySelectorAll(".filter-container");
+
+console.log(filterContainers);
+
+filterContainers.forEach((filterContainer, index) => {
+  filterContainer.addEventListener("click", (e) => openFilter(e, index));
+});
+
+function openFilter(e, index) {
+  console.log(e);
+  console.log(index);
+  console.log("openFilter ok");
+}
