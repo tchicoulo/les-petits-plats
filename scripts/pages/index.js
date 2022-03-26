7356f9bd10eb74d5dfae570dbde49c26e375dbef
Calixte <tchicoulo@hotmail.fr>
2022-03-26 19:33:38 +0100

    OpenFilter() added features

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,4 @@
-console.log("ça fonctionne");
+"use strict";
 
 const filterContainers = document.querySelectorAll(".filter-container");
 
@@ -9,7 +9,30 @@ filterContainers.forEach((filterContainer, index) => {
 });
 
 function openFilter(e, index) {
+  const ingredientElement = document.querySelector(".filter-ingredients");
+  const appliancesElement = document.querySelector(".filter-appliances");
+  const utensilsElement = document.querySelector(".filter-utensils");
+
+  const ingredientsElementList = document.querySelector(
+    ".container-ingredients"
+  );
+  const appliancesElementList = document.querySelector(".container-appliances");
+  const utensilsElementList = document.querySelector(".container-utensils");
+
+  console.log(ingredientsElementList);
+  console.log(appliancesElementList);
+  console.log(utensilsElementList);
+
   console.log(e);
   console.log(index);
-  console.log("openFilter ok");
+  if (index === 0) {
+    ingredientElement.classList.add("hide");
+    ingredientsElementList.classList.add("show");
+
+    console.log("ok index 0");
+  } else if (index === 1) {
+    console.log("ok index 1");
+  } else if (index === 2) {
+    console.log("ok index 2");
+  }
 }
