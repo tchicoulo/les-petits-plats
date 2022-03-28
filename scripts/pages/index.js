de0c0fb2f3d1dc34521d99b1dd51f2703d4fb2c2
Calixte <tchicoulo@hotmail.fr>
2022-03-28 11:57:13 +0200

    open and close filter function in progress

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,38 +1,66 @@
 "use strict";
 
 const filterContainers = document.querySelectorAll(".filter-container");
+const filterListArrowCloseButton = document.querySelectorAll(
+  ".container-list img"
+);
 
-console.log(filterContainers);
+const ingredientElement = document.querySelector(".filter-ingredients");
+const appliancesElement = document.querySelector(".filter-appliances");
+const utensilsElement = document.querySelector(".filter-utensils");
+const ingredientsElementList = document.querySelector(".container-ingredients");
+const appliancesElementList = document.querySelector(".container-appliances");
+const utensilsElementList = document.querySelector(".container-utensils");
 
 filterContainers.forEach((filterContainer, index) => {
-  filterContainer.addEventListener("click", (e) => openFilter(e, index));
+  filterContainer.addEventListener("click", () => openFilter(index));
 });
 
-function openFilter(e, index) {
-  const ingredientElement = document.querySelector(".filter-ingredients");
-  const appliancesElement = document.querySelector(".filter-appliances");
-  const utensilsElement = document.querySelector(".filter-utensils");
+filterListArrowCloseButton.forEach((filterClosebutton, index) => {
+  filterClosebutton.addEventListener("click", (e) => closeFilter(e, index));
+});
 
-  const ingredientsElementList = document.querySelector(
-    ".container-ingredients"
-  );
-  const appliancesElementList = document.querySelector(".container-appliances");
-  const utensilsElementList = document.querySelector(".container-utensils");
+document.addEventListener("click", (e) => {
+  const filterContainersList = document.querySelectorAll(".container-list");
 
-  console.log(ingredientsElementList);
-  console.log(appliancesElementList);
-  console.log(utensilsElementList);
+  // filterContainersList.forEach((containerList) => {
+  //   if (containerList.classList.contains("show")) {
+  //     console.log("yes");
+  //     closeFilter(e);
+  //   } else {
+  //     console.log("no");
+  //     e.preventDefault();
+  //   }
+  // });
+});
 
-  console.log(e);
-  console.log(index);
+function openFilter(index) {
   if (index === 0) {
     ingredientElement.classList.add("hide");
     ingredientsElementList.classList.add("show");
-
-    console.log("ok index 0");
   } else if (index === 1) {
-    console.log("ok index 1");
+    appliancesElement.classList.add("hide");
+    appliancesElementList.classList.add("show");
   } else if (index === 2) {
-    console.log("ok index 2");
+    utensilsElement.classList.add("hide");
+    utensilsElementList.classList.add("show");
+  }
+}
+
+function closeFilter(e, index) {
+  if (index === "undefined") {
+    e.preventDefault();
+  } else if (index === 0) {
+    ingredientElement.classList.remove("hide");
+    ingredientsElementList.classList.remove("show");
+    console.log(index);
+  } else if (index === 1) {
+    appliancesElement.classList.remove("hide");
+    appliancesElementList.classList.remove("show");
+    console.log(index);
+  } else if (index === 2) {
+    utensilsElement.classList.remove("hide");
+    utensilsElementList.classList.remove("show");
+    console.log(index);
   }
 }
