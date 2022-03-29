5c9ce15c2998eeb91a329360b7b528b016e64ab9
Calixte <tchicoulo@hotmail.fr>
2022-03-29 16:10:07 +0200

    added full static + animation css/js about filters

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,13 +1,19 @@
 "use strict";
 
+import { recipes } from "../../data/recipes.js";
+
+console.log(recipes);
+
 const filterContainers = document.querySelectorAll(".filter-container");
 const filterListArrowCloseButton = document.querySelectorAll(
   ".container-list img"
 );
+const filterContainersList = document.querySelectorAll(".container-list");
 
 const ingredientElement = document.querySelector(".filter-ingredients");
 const appliancesElement = document.querySelector(".filter-appliances");
 const utensilsElement = document.querySelector(".filter-utensils");
+
 const ingredientsElementList = document.querySelector(".container-ingredients");
 const appliancesElementList = document.querySelector(".container-appliances");
 const utensilsElementList = document.querySelector(".container-utensils");
@@ -21,29 +27,53 @@ filterListArrowCloseButton.forEach((filterClosebutton, index) => {
 });
 
 document.addEventListener("click", (e) => {
-  // const filterContainersList = document.querySelectorAll(".container-list");
+  const ignore = document.querySelector(".filters");
+  const target = e.target;
 
-  // filterContainersList.forEach((containerList) => {
-  //   if (containerList.classList.contains("show")) {
-  //     console.log("yes");
-  //     closeFilter(e);
-  //   } else {
-  //     console.log("no");
-  e.preventDefault();
-  //   }
-  // });
+  if (target === ignore || ignore.contains(target)) {
+    return console.log("ignore");
+  }
+
+  filterContainersList.forEach((containerList) => {
+    if (containerList.classList.contains("show")) {
+      containerList.classList.remove("show");
+    }
+  });
+  filterContainers.forEach((containerFilter) => {
+    if (containerFilter.classList.contains("hide")) {
+      containerFilter.classList.remove("hide");
+    }
+  });
 });
 
 function toggleFilter(index) {
   if (index === 0) {
     ingredientElement.classList.toggle("hide");
     ingredientsElementList.classList.toggle("show");
+
+    appliancesElement.classList.remove("hide");
+    appliancesElementList.classList.remove("show");
+
+    utensilsElement.classList.remove("hide");
+    utensilsElementList.classList.remove("show");
   } else if (index === 1) {
     appliancesElement.classList.toggle("hide");
     appliancesElementList.classList.toggle("show");
+
+    ingredientElement.classList.remove("hide");
+    ingredientsElementList.classList.remove("show");
+
+    utensilsElement.classList.remove("hide");
+    utensilsElementList.classList.remove("show");
   } else if (index === 2) {
     utensilsElement.classList.toggle("hide");
     utensilsElementList.classList.toggle("show");
+
+    appliancesElement.classList.remove("hide");
+    appliancesElementList.classList.remove("show");
+
+    ingredientElement.classList.remove("hide");
+    ingredientsElementList.classList.remove("show");
   }
 }
 
