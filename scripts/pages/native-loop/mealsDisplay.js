396ea08f7c6c81878e81159efe64d1e9e79cce02
Calixte <tchicoulo@hotmail.fr>
2022-03-31 17:03:39 +0200

    native loop in progress

--- scripts/pages/native-loop/mealsDisplay.js
+++ scripts/pages/native-loop/mealsDisplay.js
@@ -2,7 +2,28 @@ import { recipes } from "../../../data/recipes.js";
 
 console.log(recipes);
 
-const test = document.querySelector(".card-description p");
-console.log(test.innerHTML);
+const searchContainer = document.querySelector(".search-container input");
+const result = document.getElementById("result");
 
-// function
+searchContainer.addEventListener("input", (e) => {
+  mealsdisplay(e);
+});
+
+function mealsdisplay(e) {
+  if (recipes === null) {
+    result.innerHTML = `<li class="no-result">Aucun résultat</li>`;
+  }
+
+  result.innerHTML = recipes.map((recipe) => {
+    let ingredients = [];
+    let appliances = [];
+    let utensils = [];
+
+    // for (let i = 1; i < 50; i++) {
+    //   console.log(recipe);
+    // }
+    console.log(recipe.appliance);
+    console.log(recipe.ingredients);
+    console.log(recipe.ustensils);
+  });
+}
